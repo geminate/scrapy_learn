6698864cc05c8756842873d22b90cb2e2893b00b
liuhuihao <1061036802@qq.com>
2019-08-12 18:02:37 +0800

    fix: cookie 转换方法修复

--- utils.py
+++ utils.py
@@ -1,9 +1,3 @@
 # 将浏览器中的 cookie 转换为 dict 格式
 def cookie_to_dict(cookie):
-    cookie_dict = {}
-    items = cookie.split(';')
-    for item in items:
-        key = item.split('=')[0].replace(' ', '')
-        value = item.split('=')[1]
-        cookie_dict[key] = value
-    return cookie_dict
+    return dict([l.split("=", 1) for l in cookie.split("; ")])
